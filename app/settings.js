45abca699eefee4504c14025508c0c5344bd33e0
EllaxElla118 <onasogaemmanuel02@gmail.com>
2024-10-28 13:02:26 +0100

    Update settings.js

--- app/settings.js
+++ app/settings.js
@@ -1,9 +1,19 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 // Create the HTTP server
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, HTTP world!');
+  const parsedUrl = url.parse(req.url, true);
+  if(parsedUrl.page === 'settings') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, HTTP world!');
+  }
+  else {
+    res.writeHead(400, { 'Content-Type': 'text/html' });
+    res.write(parsedUrl);
+    res.end('Error!!!');    
+  }
 });
 
 // Start the server
