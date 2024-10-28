023a11d95e7bd6a22dedf7dd7c08d39919af166e
EllaxElla118 <onasogaemmanuel02@gmail.com>
2024-10-28 13:40:15 +0100

    Update settings.js

--- api/settings.js
+++ api/settings.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const path = require('path');
 
 // Create the HTTP server
 const server = http.createServer((req, res) => {
