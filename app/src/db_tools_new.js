906383d768599f09f0653a1b0060068e9f3bd623
EllaxElla118 <onasogaemmanuel02@gmail.com>
2024-11-01 21:25:38 +0100

    Create db_tools_new.js

---
+++ app/src/db_tools_new.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb+srv://Emma:6RynBIW4mbqEhEud@cluster0.gp8zi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+async function newSettingsByGid(gid) {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        const database = client.db("GroupData");
+        const collection = database.collection("settings");
+            document = {
+                gid: gid,
+                settings: defaultSettings,
+            };
+            await collection.insertOne(document);
+            return "Success";
+    } catch (error) {
+        console.error("Error:", error);
+    } finally {
+        await client.close();
+    }
+}
+
+// Export the function correctly
+module.exports = { newSettingsByGid };
