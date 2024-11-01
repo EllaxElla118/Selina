8064575bd74f87868e604c553c485c9c7188cedc
EllaxElla118 <onasogaemmanuel02@gmail.com>
2024-11-01 14:49:55 +0100

    Update settings.js

--- api/settings.js
+++ api/settings.js
@@ -6,6 +6,7 @@ const path = require('path');
 // Create the HTTP server
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
+  const queryParams = parsedUrl.query;
     res.writeHead(200, { 'Content-Type': 'text/html' });
     const filePath = path.join(__dirname, '../app/src/settings/settings.html');
     const data = fs.readFileSync(filePath, 'utf8');
